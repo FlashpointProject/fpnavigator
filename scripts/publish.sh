e1e6502f7f33aa90f420d8e1d6fa1dd3627847ef
coldice <coldic3@protonmail.com>
2021-09-17 21:36:03 +0200

    Create publish script for packaging release

---
+++ scripts/publish.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# Execute this file inside msys environment (mozilla-build)
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ROOTDIR="$(dirname "$SCRIPTPATH")"
+
+pushd $ROOTDIR > /dev/null
+
+echo "Packaging Flashpoint Browser..."
+echo "$PWD"
+
+echo "Configuring..."
+mach configure
+
+echo "Packaging..."
+mach package
+
+echo "Release should now be complete... see directory obj*/dist"
+echo "Finished!"
+popd > /dev/null
